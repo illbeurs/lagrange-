8a77dd07650c20ad3d261a5e81e7cb9da1dbab8c
Maxim <m.blyzen@mail.ru>
2023-11-14 20:43:41 +0300

    version without poly slide

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,71 +1,62 @@
 import androidx.compose.desktop.ui.tooling.preview.Preview
 import androidx.compose.foundation.Canvas
-import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.gestures.detectTapGestures
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.*
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
+import androidx.compose.runtime.*
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.PointMode
 import androidx.compose.ui.input.pointer.PointerEventType
 import androidx.compose.ui.input.pointer.onPointerEvent
-import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.text.ExperimentalTextApi
-import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.drawText
 import androidx.compose.ui.text.rememberTextMeasurer
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
-import java.awt.MouseInfo
-import java.awt.Point
-import java.awt.event.MouseEvent
-
 
 @OptIn(ExperimentalTextApi::class, ExperimentalComposeUiApi::class)
 @Composable
 @Preview
 fun App() {
     var xMin by remember { mutableStateOf(-10) }
-    var yMin by remember { mutableStateOf(0) }
+    val yMin by remember { mutableStateOf(0) }
     var xMax by remember { mutableStateOf( 10) }
     var yMinMax by remember{mutableStateOf(0f)}
     var selectY by remember{mutableStateOf(true)}
+    var build by remember{mutableStateOf(false)}
+    var y0 by remember{mutableStateOf(0f)}
     val textMeasurer = rememberTextMeasurer()
-    var points by remember { mutableStateOf(mutableMapOf<Float, Float>()) }
-    Canvas(modifier = Modifier.fillMaxSize().clickable{}.
+    val polypoints by  remember{mutableStateOf(mutableListOf<Offset>())}
+    val points by remember { mutableStateOf(mutableListOf<Pair<Float, Float>>())}
+    var can = Canvas(modifier = Modifier.fillMaxSize().clickable{}.
         onPointerEvent(PointerEventType.Press){
+            polypoints.clear()
             var point = it.changes.first().position
-            points[point.x] = point.y
+            points.add(Pair(point.x, point.y))
         },
         onDraw = {
             var yMax = this.size.height*(xMax-xMin)/this.size.width+yMin
-            if(yMax*yMin<0){
-                yMinMax = ((xMax-xMin)/(xMax+xMin)).toFloat()
-            }
             //ось OX
             drawLine(
                 color = Color.Black,
                 start = Offset(0f, this.size.height * (1 + yMinMax) / 2),
                 end = Offset(this.size.width, this.size.height * (1 + yMinMax) / 2)
             )
+            y0 = this.size.height * (1 + yMinMax) / 2
             //ось OY
             drawLine(color = Color.Black,
                 start = Offset(-this.size.width*xMin/(xMax-xMin), 0f),
                 end = Offset(-this.size.width*xMin/(xMax-xMin), this.size.height))
-            for(point in points) {
+            for((x,y) in points) {
                 drawCircle(
                     color = Color.Green,
                     radius = 10f,
-                    center = Offset(point.key, point.value)
+                    center = Offset(x, y)
                 )
             }
             for(i in xMin .. xMax) {
@@ -75,9 +66,22 @@ fun App() {
                     end = Offset(this.size.width*(i-xMin)/(xMax-xMin),
                         this.size.height*(1+yMinMax)/2+5))
                 drawText(textMeasurer = textMeasurer, text = i.toString(),
-                    topLeft = Offset(this.size.width*(i-xMin)/(xMax-xMin)-5,
-                        this.size.height*(1+yMinMax)/2))
+                    topLeft = Offset(this.size.width*(i-xMin)/(xMax-xMin)-7,
+                        this.size.height*(1+yMinMax)/2+6))
             }
+            if (build){
+                for (x in 0..size.width.toInt()) {
+                    val y = BuildPoly(x.toFloat(),points)
+                    polypoints.add(Offset(x.toFloat(), y))
+                }
+                drawPoints(
+                    points = polypoints,
+                    strokeWidth = 3f,
+                    pointMode = PointMode.Points,
+                    color = Color.Blue
+                )
+            }
+
         })
 
     Column(modifier = Modifier.fillMaxHeight(), verticalArrangement = Arrangement.Bottom) {
@@ -90,7 +94,7 @@ fun App() {
             Column(modifier = Modifier.padding(10.dp, 10.dp)) {
                 Text("xMax")
                 TextField(value = xMax.toString(),
-                    onValueChange = { value -> xMax = value.toIntOrNull() ?: 0 })
+                    onValueChange = { value -> xMax = value.toIntOrNull() ?: 10 })
             }
             Column{
                 Row{
@@ -119,14 +123,67 @@ fun App() {
                         onValueChange = { yMinMax = it })
                 }
                 else{
-                    TextField(value = xMin.toString(),
-                        onValueChange = { value -> yMin = value.toIntOrNull() ?: 0 })
+                    TextField(value = yMinMax.toString(),
+                        onValueChange = { value -> yMinMax = value.toFloatOrNull() ?: 0f })
                 }
             }
         }
     }
+    Box(modifier = Modifier.padding(50.dp, 50.dp)) {
+        // ... Ваш существующий код
+
+        // Создаем кнопку
+        val button = Button(
+            onClick = {
+                build = true
+            },
+            modifier = Modifier,
+            contentPadding = PaddingValues(horizontal = 50.dp, vertical = 20.dp)
+        ) {
+            Text("Build")
+        }
+
+        // Размещаем кнопку внутри Column
+        button
+    }
+    Box(modifier = Modifier.padding(300.dp, 50.dp)) {
+        // ... Ваш существующий код
+
+        // Создаем кнопку
+        val button = Button(
+            onClick = {
+                      build = false
+                      points.clear()
+                      polypoints.clear()
+            },
+            modifier = Modifier,
+            contentPadding = PaddingValues(horizontal = 50.dp, vertical = 20.dp)
+        ) {
+            Text("Clear")
+        }
+
+        // Размещаем кнопку внутри Column
+        button
+    }
+
 }
 
+fun lk(x:Float,xk:Float,points: MutableList<Pair<Float, Float>>):Float{
+    var l = 1f;
+    for ((xi,y) in points){
+        if (xi==xk){continue}
+        l*=(x-xi)/(xk-xi)
+    }
+    return l
+}
+
+fun BuildPoly(x:Float, points: MutableList<Pair<Float, Float>>):Float{
+    var summ: Float = 0f
+    for ((xk,yk) in points) {
+        summ+= yk*lk(x,xk,points)
+    }
+    return summ
+}
 fun main() = application {
     Window(onCloseRequest = ::exitApplication) {
         App()
